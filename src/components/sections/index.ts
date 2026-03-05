18c7d75bd6d4fd9de1b333499415b5d840f9c0e9
Nebiyu Esaiyas <nebiyuesaiyas1954@gmail.com>
2026-03-05 16:36:24 +0300

    Add sections barrel exports
    Create src/components/sections/index.ts to re-export section components and their types from './Hero' and './Services' (HeroSection, TrustedBySection, OurServicesSection and associated props/types). This centralizes imports for sections and simplifies consumer code.

---
+++ src/components/sections/index.ts
@@ -0,0 +1,5 @@
+export { HeroSection } from './Hero';
+export { TrustedBySection } from './Hero';
+export { OurServicesSection } from './Services';
+export type { HeroAction, HeroSectionProps, TrustedBySectionProps, TrustedPartnerLogo } from './Hero';
+export type { OurServicesSectionProps, ServiceItem } from './Services';
