d71ab198ac456ddabb7845ff1e6369645c969404
Nebiyu Esaiyas <nebiyuesaiyas1954@gmail.com>
2026-03-22 02:14:10 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,19 @@ export default {
       colors: {
         primary: '#282973',
         secondary: '#008080',
-        background: '#fefaf5',
+
+
+        background: '#f9fcfc',
+        backgroundOne: '#dfdfea',
+        backgroundDark: '#0e0e28',
+        backgroundDarkOne: '#1e293b',
+
+        servicesCardDark: '#0e0e28',
+        servicesCardLight: '#f9fcfc',
+
+
+        a: '#e6f2f2',
+        b: '#e6f2f2',
       },
       fontFamily: {
         display: ['Inter', 'ui-sans-serif', 'system-ui', 'sans-serif'],
@@ -16,3 +28,6 @@ export default {
   },
   plugins: [],
 };
+
+
+// 1, dfdfea 2, d9ecec  
