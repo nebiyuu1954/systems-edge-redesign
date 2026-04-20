ae0fe6f660ccebf7a2fb9323f175811c8c1e7eba
Nebiyu Esaiyas <nebiyuesaiyas1954@gmail.com>
2026-04-20 15:56:50 +0300

    Update index.ts

--- src/components/sections/index.ts
+++ src/components/sections/index.ts
@@ -1,10 +1,12 @@
 export { HeroSection } from './Hero';
 export { TrustedBySection } from './Hero';
 export { default as NumbersSection } from './Numbers/NumbersSection';
+export { FAQSection } from './FAQ';
 export { OurServicesSection } from './Services';
 export { ObjectivesSection } from './Objectives';
-export { default as TestimonialsSection } from './Testimonials/TestimonialsSection';
+export { default as OurPartnersSection } from './Testimonials/TestimonialsSection';
 export type { HeroAction, HeroSectionProps, TrustedBySectionProps, TrustedPartnerLogo } from './Hero';
+export type { FAQItem, FAQSectionProps } from './FAQ';
 export type { OurServicesSectionProps, ServiceItem } from './Services';
 export type { ObjectiveBlock, ObjectivesSectionProps } from './Objectives';
 export { TestingSection } from './Services/Testing';
